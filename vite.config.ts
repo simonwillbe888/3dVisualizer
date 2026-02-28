aafd6b226ddf5d047aa94786d98fe90a715cce78
dengzhiwen <dengzhiwen@ncti-gba.cn>
2026-02-28 10:06:40 +0800

    ✨简化配置 移除APIkey

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,20 @@
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig } from 'vite';
 
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
-    const isProduction = mode === 'production';
-
-  return {
-    plugins: [vue(), tailwindcss()],
-        base:  '/3dVisualizer/',
-
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+export default defineConfig({
+  plugins: [vue(), tailwindcss()],
+  base: '/3dVisualizer/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
     },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
-    server: {
- 
-      hmr: process.env.DISABLE_HMR !== 'true',
-    },
-      build: {
-      outDir: 'dist',
-    },
-  };
+  },
+  server: {
+    hmr: process.env.DISABLE_HMR !== 'true',
+  },
+  build: {
+    outDir: 'dist',
+  },
 });
