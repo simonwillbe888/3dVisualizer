2f1bc0683bbd33f6a5255ad7fa92b888d87fce60
dengzhiwen <dengzhiwen@ncti-gba.cn>
2026-02-28 09:10:05 +0800

    ✨github actions

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,12 @@ import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
+    const isProduction = mode === 'production';
+
   return {
     plugins: [vue(), tailwindcss()],
+        base:  '/3dVisualizer/',
+
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
@@ -16,9 +20,11 @@ export default defineConfig(({mode}) => {
       },
     },
     server: {
-      // HMR is disabled in AI Studio via DISABLE_HMR env var.
-      // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
+ 
       hmr: process.env.DISABLE_HMR !== 'true',
+    },
+      build: {
+      outDir: 'dist',
     },
   };
 });
